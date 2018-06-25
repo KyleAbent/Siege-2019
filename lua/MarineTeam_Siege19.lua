2f2631f224369ce97b96c8e7ff956183efdabe02
Kyle <kyleabent@gmail.com>
2018-06-25 03:07:05 -0400

    dsd
    fdf

--- lua/MarineTeam_Siege19.lua
+++ lua/MarineTeam_Siege19.lua
@@ -13,7 +13,7 @@ function MarineTeam:InitTechTree()
     self.techTree:AddResearchNode(kTechId.ExtractorArmor1,     kTechId.Armor3, kTechId.None)
     self.techTree:AddResearchNode(kTechId.JetpackFuel1,     kTechId.JetpackTech, kTechId.None)
     self.techTree:AddActivation(kTechId.SiegeBeacon,           kTechId.Observatory)  
-    self.techTree:AddActivation(kTechId.UnlockAdvancedBeacon, kTechId.Observatory) 
+    self.techTree:AddResearchNode(kTechId.UnlockAdvancedBeacon, kTechId.Observatory) 
     self.techTree:AddActivation(kTechId.AdvancedBeacon, kTechId.UnlockAdvancedBeacon) 
     self.techTree:AddResearchNode(kTechId.RunSpeed1,     kTechId.InfantryPortal, kTechId.Armor3) --I'm proud of this!
     self.techTree:AddResearchNode(kTechId.MacDefenseBuff,     kTechId.RoboticsFactory, kTechId.MAC) --I'm proud of this!
